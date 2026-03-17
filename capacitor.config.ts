e423c919f9d7b48c67ef7504ab61158a9e827f5e
Mahesh R M <maheshrmadiwalar@gmail.com>
2026-03-17 23:27:50 +0530

    Auto update: 2026-03-17 23:27:50

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nammacraft.app',
   appName: 'NammaCraft',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#4A2B20",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
